8ab364f7d1a759da9c90ce1924fbb7923ef0e9bb
Lezn0 <yuxinwu1234@gmail.com>
2020-08-22 19:11:49 +0800

    added echo function with addLines function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,33 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static String addLines(String input){
+        return "____________________________________________________________\n" +
+                input+
+                "\n____________________________________________________________\n";
+    }
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String greetMsg = addLines(" Hello! I'm Duke\n" + " What can I do for you?");
+        String byeMsg = addLines(" Bye. Hope to see you again soon!");
         System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________\n" +
-                " Hello! I'm Duke\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________");
+        System.out.println(greetMsg);
+
+        while(true) {
+            String input = in.nextLine();
+            if (!input.equals("bye")) {
+                String line = addLines(input);
+                System.out.println(line);
+            } else {
+                break;
+            }
+        }
+        System.out.println(byeMsg);
     }
 }
