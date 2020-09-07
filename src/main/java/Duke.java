7bde1a1d8da06e26f486cd83fc5a17abf7317bc3
Lezn0 <yuxinwu1234@gmail.com>
2020-09-07 15:31:06 +0800

    Add method to handle errors
    Modify event output from "[D]" to "[E]" and "by:" to "at:"

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
 
 public class Duke {
-
+    private static final List LIST = new List();
+    private static final Scanner IN = new Scanner(System.in);
     public static void main(String[] args) {
-        List list = new List();
-        Scanner in = new Scanner(System.in);
         String input= "";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -14,31 +13,38 @@ public class Duke {
 
         System.out.println("Hello from\n" + logo);
         addLines("Hello! I'm Duke\n" + "What can I do for you?");
+        runCommand(input);
+    }
 
+    private static void runCommand(String input) {
         while(!input.equals("bye")) {
-            input = in.nextLine();
-            String[] splitInput = input.split(" ", 2);
-            switch(splitInput[0]){
-            case "bye":
-                addLines("Bye. Hope to see you again soon!");
-                break;
-            case "list":
-                list.queryItems();
-                break;
-            case "done":
-                list.completeTask(Integer.parseInt(splitInput[1])-1);
-                break;
-            case "todo":
-                list.addItem(splitInput[1]);
-                break;
-            case "deadline":
-                list.addDeadline(splitInput[1]);
-                break;
-            case "event":
-                list.addEvent(splitInput[1]);
-                break;
-            default:
-                addLines("invalid input!");
+            input = IN.nextLine();
+            try {
+                String[] splitInput = input.split(" ", 2);
+                switch(splitInput[0]){
+                case "bye":
+                    addLines("Bye. Hope to see you again soon!");
+                    break;
+                case "list":
+                    LIST.queryItems();
+                    break;
+                case "done":
+                    LIST.completeTask(splitInput[1]);
+                    break;
+                case "todo":
+                    LIST.addItem(splitInput[1]);
+                    break;
+                case "deadline":
+                    LIST.addDeadline(splitInput[1]);
+                    break;
+                case "event":
+                    LIST.addEvent(splitInput[1]);
+                    break;
+                default:
+                    addLines("invalid command!");
+                }
+            } catch (Exception e) {
+                addLines("Please enter a valid action after the command");
             }
         }
     }
--- src/main/java/List.java
+++ src/main/java/List.java
@@ -1,6 +1,6 @@
 public class List {
 
-    private Task[] items;
+    private static Task[] items;
     private int size;
 
     List(){
@@ -9,56 +9,88 @@ public class List {
     }
 
     public void queryItems(){
-        System.out.println("____________________________________________________________");
+        printLine();
         System.out.println("Here are the tasks in your list:");
         for(int i=0; i<size; i++) {
             System.out.println(i+1 + ". " + items[i]);
         }
-        System.out.println("____________________________________________________________");
+        printLine();
     }
 
-    public void completeTask(int index){
-        System.out.println("____________________________________________________________");
-        if(items[index]!=null && index<100) {
+    public void completeTask(String input){
+        int index;
+        printLine();
+        try{
+            index=Integer.parseInt(input)-1;
             items[index].markAsDone();
             System.out.println("Nice! I've marked this task as done:\n"+items[index]);
-        } else {
+        } catch (Exception e){
             System.out.println("invalid index, try again");
         }
-        System.out.println("____________________________________________________________");
+        printLine();
     }
 
     public void addItem(String item){
-        Task newTask = new Task(item.trim());
-        items[size] = newTask;
-        size++;
-        itemAddedMessage(newTask);
+        try {
+            Task newTask = new Task(item.trim());
+            items[size] = newTask;
+            size++;
+            itemAddedMessage(newTask);
+        } catch (Exception e) {
+            printLine();
+            System.out.println("invalid input, try again");
+            printLine();
+        }
     }
 
     public void addDeadline(String input){
-        String[] deadlineInputs = input.split("/by",2);
-        Deadline newDeadline= new Deadline(deadlineInputs[0].trim(),deadlineInputs[1].trim());
-        items[size] = newDeadline;
-        size++;
-        itemAddedMessage(newDeadline);
+        try{
+            String[] deadlineInputs = input.split("/by",2);
+            Deadline newDeadline= new Deadline(deadlineInputs[0].trim(),deadlineInputs[1].trim());
+            items[size] = newDeadline;
+            size++;
+            itemAddedMessage(newDeadline);
+        } catch (IndexOutOfBoundsException e) {
+            printLine();
+            System.out.println("Please input a valid deadline using /by");
+            printLine();
+        } catch (Exception e) {
+            printLine();
+            System.out.println("invalid input, try again");
+            printLine();
+        }
     }
 
     public void addEvent(String input){
-        String[] eventInputs = input.split("/at",2);
-        Event newEvent= new Event(eventInputs[0].trim(),eventInputs[1].trim());
-        items[size] = newEvent;
-        size++;
-        itemAddedMessage(newEvent);
+        try {
+            String[] eventInputs = input.split("/at",2);
+            Event newEvent= new Event(eventInputs[0].trim(),eventInputs[1].trim());
+            items[size] = newEvent;
+            size++;
+            itemAddedMessage(newEvent);
+        } catch (IndexOutOfBoundsException e) {
+            printLine();
+            System.out.println("Please input a valid time for the event using /at");
+            printLine();
+        } catch (Exception e) {
+            printLine();
+            System.out.println("invalid input, try again");
+            printLine();
+        }
+    }
+
+    private void printLine(){
+        System.out.println("____________________________________________________________");
     }
 
     private void itemAddedMessage(Task item){
-        System.out.println("____________________________________________________________\n" +
-                "Got it. I've added this task:\n" + item);
+        printLine();
+        System.out.println("Got it. I've added this task:\n" + item);
         if(size>1) {
             System.out.println("Now you have " + size + " tasks in the list.");
         } else{
             System.out.println("Now you have " + size + " task in the list.");
         }
-        System.out.println("____________________________________________________________");
+        printLine();
     }
 }
